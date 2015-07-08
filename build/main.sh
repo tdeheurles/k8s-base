040b46ccb7c6dd40cea84c6e80b4f69880b99ebf
tdeheurles <tdeheurles@gmail.com>
2015-07-08 12:40:30 +0200

    Fix error with sed

--- build/main.sh
+++ build/main.sh
@@ -4,6 +4,7 @@
 # Control if local build and prepare for it if needed
 . ./config/localbuild.cfg 2&>/dev/null
 if [[ -n $local_build ]]; then
+  echo -e "\e[96mmain.sh:\e[39m build will be local"
   . ./build/localbuildnumber 2&>/dev/null
 
   if [[ -z $LATEST_BUILD_NUMBER ]]; then
@@ -18,23 +19,25 @@ fi
 # name artifact
 artifact_name="gcr.io/$projectid/$servicename"
 artifact_tag="$artifact_name:$servicemajor.$serviceminor.$BUILD_NUMBER"
-
+echo -e "\e[96mmain.sh:\e[39m artifact will be : $artifact_tag"
 
 # Prepare container
+echo -e "\e[96mmain.sh:\e[39m preparing container"
 mkdir -p ./build/container
 sed "s/__SERVICEPORT__/$serviceport1/g" ./build/template.Dockerfile > ./build/container/Dockerfile
 cp ./src/*    ./build/container/
 docker build -t $artifact_name ./build/container/
 docker tag -f $artifact_name $artifact_tag
 
-
 # Push to Google Cloud Engine
 if [[ -z $local_build ]]; then
+  echo -e "\e[96mmain.sh:\e[39m Pushing to Google Cloud Container Registry"
   gcloud docker push $artifact_name
   gcloud docker push $artifact_tag
 fi
 
 # generate manifests
+echo -e "\e[96mmain.sh:\e[39m Generating manifests"
 deployPath=./deploy/kubernetes
 rm $deployPath/*.json 2&>/dev/null
 rm $deployPath/*.yml 2&>/dev/null
--- deploy/kubernetes/createManifests.sh
+++ deploy/kubernetes/createManifests.sh
@@ -11,26 +11,26 @@ rcname=$servicename
 
 sed "s/__rcName__/$rcname/g" \
     $manifestsPath/$k8s_api_version/rc.template.$template_extension > $rcfile
-sed -i "s/__major__/$servicemajor/g" $rcfile
-sed -i "s/__minor__/$serviceminor/g" $rcfile
-sed -i "s/__build__/$BUILD_NUMBER/g" $rcfile
-sed -i "s|__image__|$1|g" $rcfile
-sed -i "s/__privatePortName__/$serviceportname/g" $rcfile
-sed -i "s/__privatePort__/$serviceport/g" $rcfile
-sed -i "s/__replicas__/1/g" $rcfile
+sed "s/__major__/$servicemajor/g" $rcfile > $rcfile
+sed "s/__minor__/$serviceminor/g" $rcfile > $rcfile
+sed "s/__build__/$BUILD_NUMBER/g" $rcfile > $rcfile
+sed "s|__image__|$1|g" $rcfile > $rcfile
+sed "s/__privatePortName__/$serviceportname/g" $rcfile > $rcfile
+sed "s/__privatePort__/$serviceport/g" $rcfile > $rcfile
+sed "s/__replicas__/1/g" $rcfile > $rcfile
 
 
 # SERVICE
 servicefile=$manifestsPath/$servicename\_$versiontag\_service.$template_extension
 sed "s/__serviceName__/$servicename/g" \
     $manifestsPath/$k8s_api_version/service.template.$template_extension > $servicefile
-sed -i "s/__major__/$servicemajor/g" $servicefile
-sed -i "s/__minor__/$serviceminor/g" $servicefile
-sed -i "s/__build__/$BUILD_NUMBER/g" $servicefile
-sed -i "s/__privatePortName__/$serviceportname/g" $servicefile
-sed -i "s/__publicPortName__/$serviceportname/g" $servicefile
-sed -i "s/__publicPort__/$serviceport/g" $servicefile
-sed -i "s/__rcName__/$rcname/g" $servicefile
+sed "s/__major__/$servicemajor/g" $servicefile > $servicefile
+sed "s/__minor__/$serviceminor/g" $servicefile > $servicefile
+sed "s/__build__/$BUILD_NUMBER/g" $servicefile > $servicefile
+sed "s/__privatePortName__/$serviceportname/g" $servicefile > $servicefile
+sed "s/__publicPortName__/$serviceportname/g" $servicefile > $servicefile
+sed "s/__publicPort__/$serviceport/g" $servicefile > $servicefile
+sed "s/__rcName__/$rcname/g" $servicefile > $servicefile
 
 cp $rcfile $manifestsPath/rc_latest.$template_extension
 cp $servicefile $manifestsPath/service_latest.$template_extension
